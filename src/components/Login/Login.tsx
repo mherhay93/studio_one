48b4d4b963eb72775a7710e9e3ff5a5dea0386be
mherhay93 <mherhay93@gmail.com>
2023-02-07 17:43:28 +0400

    add login page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,12 +1,36 @@
+import {Form, Formik, Field} from "formik";
+import {Link} from "react-router-dom";
+import {pages} from "../../constants";
 import classes from './styleLogin.module.css'
 
 const Login = () => {
 
   return (
-  <div>
-
+  <div className={classes.container}>
+    <div className={classes.containerForm}>
+      <h3 className={classes.title}>Login</h3>
+      <Formik
+      initialValues={{name: '', password: ''}}
+      onSubmit={(values) => {
+        console.log(values)
+      }}
+      >
+        <Form>
+          <div className={classes.input}>
+            <label className={classes.label} htmlFor="name">Name</label>
+            <Field id="name" name="name" placeholder="Name"/>
+          </div>
+          <div className={classes.input}>
+            <label className={classes.label} htmlFor="password">Password</label>
+            <Field id="password" name="password" placeholder="Password"/>
+          </div>
+          <button className={classes.button} type="submit">Login</button>
+        </Form>
+      </Formik>
+    </div>
+    <Link to={pages[0].path} className={classes.home}>Home</Link>
   </div>
-  )
+  );
 }
 
 export default Login
